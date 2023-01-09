77a493399a1fcb920b395742c99671311b042ed1
daeuun <leeda51925@gmail.com>
2023-01-09 23:17:13 +0900

    greedy

---
+++ greedy/adventuregruop.py
@@ -0,0 +1,14 @@
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+
+result = 0  # 총그룹
+count = 0  # 현재 그룹의 모험가
+
+for i in data:
+    count += 1
+    if count > i:
+        result += 1  # 현재 공포도 이상이면 총 그룹의 수 증가
+        count = 0
+
+print(result)
